be02fe3b80954f4b57a26fe5659722f571ecd858
kolhesatish <129759178+kolhesatish@users.noreply.github.com>
2025-03-13 15:42:06 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://open-comment-backend-2.vercel.app",
         changeOrigin: true,
       },
     },
